71d6a850fdaa323d44fc829043d896e89f678e19
zhuyue <15129310565@163.com>
2018-05-22 20:28:17 -0700

    jichu

--- redux/store.js
+++
@@ -1,5 +0,0 @@
-// import {createStore} from 'redux'
-import {createStore} from '../libs/redux'
-import reducers from './reducers'  // reducers包含多个reducer的reducer
-
-export default createStore(reducers)
